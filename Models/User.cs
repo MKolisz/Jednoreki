877d7da7c912bf3716f544306d52474bbcb92f31
DESKTOP-S1IQFSU\mkolisz11 <mkolisz11@gmail.com>
2020-01-28 12:13:50 +0100

    User model and controller, connection to database

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Jednoreki.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Balance { get; set; }
+        public int TimePlayed { get; set; }
+    }
+}
